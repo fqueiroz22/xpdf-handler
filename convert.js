25dc67265b327f1a7a90033a0fd7f33aed917edb
Felipe Queiroz <felipe.queiroz@fincs.com.br>
2019-07-05 15:50:00 -0300

    index.js

--- convert.js
+++
@@ -1,7 +0,0 @@
-const execFile = require('child_process').execFile
-
-  runExe('pdftotext', ['-table', "./extrato.pdf"], function(err, data) {
-    if (err) {
-      console.log(err)
-    } else console.log('foi')
-  })
---
+++ index.js
@@ -0,0 +1,7 @@
+const runExe = require('child_process').execFile
+
+  runExe('pdftotext', ['-table', "./extrato.pdf"], function(err, data) {
+    if (err) {
+      console.log(err)
+    } else console.log('foi')
+  })
