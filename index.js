53a778085ef899e05f4faf3126338f001b5f69e6
fqueiroz22 <felipe.queiroz@fincs.com.br>
2019-07-10 11:09:37 -0300

    -layout

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function convertPDF(path){
   var runExe = require('child_process').execFile
 
-  runExe('pdftotext', ['-table', path], function(err, data) {
+  runExe('pdftotext', ['-layout', path], function(err, data) {
     if (err) {
       console.log(err)
     } else console.log('foi')
