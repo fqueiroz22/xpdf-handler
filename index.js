fc39999982689745e8ba8c4f189c78489872694f
fqueiroz22 <felipe.queiroz@fincs.com.br>
2019-07-11 14:36:50 -0300

    require.spawn

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 function convertPDF(path){
-  var runExe = require('child_process').execFile
+  var runExe = require('child_process').spawn
 
-  runExe('pdftotext', ['-layout', path], function(err, data) {
-    if (err) {
-      console.log(err)
-    } else console.log('foi')
-  })
+  runExe('pdftotext', ['-layout', path])
+
+  // var runExe = require('child_process').execFile
+
+  // runExe('pdftotext', ['-layout', path], function(err, data) {
+  //   if (err) {
+  //     console.log(err)
+  //   } else console.log('foi')
+  // })
 }
 
-// convertPDF('./extrato.pdf')
+convertPDF('./extrato.pdf')
